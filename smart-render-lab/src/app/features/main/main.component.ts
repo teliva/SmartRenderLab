0046790d61219c52351dd770ece5893e04c43293
Ivan Telitsyn <itelitsyn@kisp.com>
2026-02-09 13:38:35 -0500

    finalized the project xml feature

--- smart-render-lab/src/app/features/main/main.component.ts
+++ smart-render-lab/src/app/features/main/main.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import format from 'xml-formatter';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-main',
@@ -6,6 +8,37 @@ import { Component } from '@angular/core';
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  private modalService = inject(NgbModal);
+  public formattedXml: string = "";
+  public rawXml: string = "";
+  
+  constructor() { }
+  
+  ngOnInit() { }
+
+  onInputChange(event: Event) {
+    const ele = event.target as HTMLInputElement;
+    this.rawXml = ele.value;
+    this.formattedXml = this.beautify(ele.value);
+  }
+
+  open(content: any) {
+    this.modalService.open(content);
+  }
+
+  beautify(rawXml: string): string {
+    try {
+      return format(rawXml, {
+        indentation: '  ', // Two spaces
+        filter: (node) => true, // Optional: filter nodes
+        collapseContent: true, // Keeps short tags on one line
+        lineSeparator: '\n'
+      });
+    } catch (e) {
+      console.error("Invalid XML provided", e);
+      return "Error parsing XML.";
+    }
+  }
 
 }
